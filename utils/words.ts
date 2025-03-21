81510ca957a7b979988c4077b29e8a114ac4ebca
tsegaye27 <ttalegn29@gmail.com>
2025-03-21 22:30:35 +0300

    fix: Update random word API endpoint

--- utils/words.ts
+++ utils/words.ts
@@ -3,7 +3,7 @@ import words from "@/assets/words.json";
 export async function getRandomWord() {
   try {
     const response = await fetch(
-      "https://random-word-api.herokuapp.com/word?length=5",
+      "https://random-word-api.vercel.app/api?words=1&length=5",
     );
     const data = await response.json();
     return data[0].toUpperCase();

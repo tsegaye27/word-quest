04b33ee5100e30d9a1b746fcf64f869e782d476b
tsegaye27 <ttalegn29@gmail.com>
2025-03-21 07:21:18 +0300

    refactor: improve checkGuess function for clarity

--- utils/checkGuess.ts
+++ utils/checkGuess.ts
@@ -1,11 +1,12 @@
 export const checkGuess = (guess: string, target: string) => {
+  const targetLetters = target.split("");
   return guess.split("").map((letter, index) => {
-    if (letter === target[index]) {
-      return { letter, color: "green" };
-    } else if (target.includes(letter)) {
-      return { letter, color: "yellow" };
+    if (letter === targetLetters[index]) {
+      return { letter, color: "#4CAF50" };
+    } else if (targetLetters.includes(letter)) {
+      return { letter, color: "#FFC107" };
     } else {
-      return { letter, color: "gray" };
+      return { letter, color: "#757575" };
     }
   });
 };

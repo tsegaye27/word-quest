d40ecf1d73f54398369993ae58a54ecf24d916a6
tsegaye27 <ttalegn29@gmail.com>
2025-03-21 23:28:38 +0300

    feat: Implement fallback to local words on API failure

--- utils/words.ts
+++ utils/words.ts
@@ -1,13 +1,21 @@
-import words from "@/assets/words.json";
+import * as words from "@/assets/words.json";
 
 export async function getRandomWord() {
   try {
     const response = await fetch(
       "https://random-word-api.vercel.app/api?words=1&length=5",
     );
+
+    if (!response.ok) {
+      console.error(`HTTP error! status: ${response.status}`);
+      throw new Error("Fetch failed");
+    }
+
     const data = await response.json();
+    console.log("Fetched word:", data[0]);
     return data[0].toUpperCase();
   } catch (error) {
+    console.error("Fetch error:", error);
     const randomIndex = Math.floor(Math.random() * words.length);
     return words[randomIndex];
   }
